203d304272c35e3b71a90c91eac09e03f6989546
Sam Littlefair <sam.littlefair@prismic.io>
2021-09-08 15:19:23 +0100

    Convert preview to component

---
+++ src/routes/preview.svelte
@@ -0,0 +1,10 @@
+<script type="module">
+  import Prismic from '@prismicio/client'
+  const apiEndpoint = 'https://svelte-tutorial.cdn.prismic.io/api/v2'
+  const linkResolver = (doc) => '/' + doc.uid
+
+  export function load({session}) {
+    console.log(session)
+  }
+
+</script>
